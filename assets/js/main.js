f73e03a0595075819a0be11f1f262da639b1ee34
Øyvind Wagner <oyvind.wagner@gmail.com>
2020-06-16 13:38:43 +0200

    Added CSS comments. Started API integration.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
+const api = {
+    key: "7b35951772455c0839a9b9d407eec07a",
+    baseurl: "http://api.openweathermap.org/data/2.5/"
+}
 
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+        getResults(searchbox.value);
+    }
+}
+
+function getResults (query) {
+    fetch(fetch(`${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`))
+}
