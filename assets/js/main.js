24701859a77f96d57fb7ba3064528982f98c095e
Øyvind Wagner <oyvind.wagner@gmail.com>
2020-06-17 11:02:08 +0200

    Updated readme. Cleaned up code.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 // API
 const api = {
-    key: "df6068444656f40d3677f016f0d0aee1",
-    base: "https://api.openweathermap.org/data/2.5/"
-}
-// Trigger
+        key: "df6068444656f40d3677f016f0d0aee1",
+        base: "https://api.openweathermap.org/data/2.5/"
+    }
+    // Trigger
 const searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress', setQuery);
 
@@ -13,40 +13,39 @@ function setQuery(evt) {
     }
 }
 //API Call
-function getResults (query) {
+function getResults(query) {
     fetch(`${api.base}/weather?q=${query}&units=metric&APPID=${api.key}`)
         .then(weather => {
             return weather.json();
         }).then(displayResults);
 }
 // Website functions
-function displayResults (weather) {
+function displayResults(weather) {
     let city = document.querySelector('.location .city');
     city.innerText = `${weather.name}, ${weather.sys.country}`;
-  
+
     let now = new Date();
     let date = document.querySelector('.location .date');
     date.innerText = dateBuilder(now);
-  
+
     let temp = document.querySelector('.current-weather .temp');
     temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
-  
+
     let weather_el = document.querySelector('.current-weather .weather');
     weather_el.innerText = weather.weather[0].main;
-  
+
     let hilow = document.querySelector('.hi-low');
     hilow.innerText = `${Math.round(weather.main.temp_min)}°C Low | ${Math.round(weather.main.temp_max)}°C High`;
-  }
-  
-  function dateBuilder (d) {
+}
+
+function dateBuilder(d) {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  
+
     let day = days[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonth()];
     let year = d.getFullYear();
-  
+
     return `${day} ${date} ${month} ${year}`;
-  }
-  
+}
